aa543023cbe2781fb8625875143da6c350c0eaba
Esther Olatunde <olatundeesther@gmail.com>
2015-06-25 08:29:48 +0000

    redirect from home to popular

--- js/app.js
+++ js/app.js
@@ -4,5 +4,6 @@ var app = angular.module('esteebble', ['ngRoute', 'esteebble.controllers']);
 
 app.config(function ($routeProvider) {
 	$routeProvider
-	.when("/:list", {controller:'ShotsListCtrl', templateUrl: "partials/shots_list.html"});
+	.when("/:list", {controller:'ShotsListCtrl', templateUrl: "partials/shots_list.html"})
+	.otherwise({redirectTo: "/popular"});
 });
